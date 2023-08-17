6fb50a9c38723ee2a865b9c9785165bff73a05fd
Iftikhar <iftikhar.mahmud101@gmail.com>
2023-08-17 23:55:35 +0600

    Added Different Slots

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ function addExam() {
         date = dateParts[2] + "-" + dateParts[1] + "-" + dateParts[0];
     }
     var name = document.getElementById('examName').value;
+    var slot = document.getElementById('examSlot').value;
+    
     if (date && name) {
         var tableBody = document.getElementById('tableBody');
         var row = tableBody.insertRow();
@@ -12,14 +14,24 @@ function addExam() {
         var cell2 = row.insertCell(1);
         var cell3 = row.insertCell(2);
         var cell4 = row.insertCell(3);
+        var cell5 = row.insertCell(4);
         cell1.innerHTML = date;
-        cell2.innerHTML = name;
-        cell3.innerHTML = '<button class="table-button edit-button" onclick="editExam(this)">✎</button>'; // Edit button
-        cell4.innerHTML = '<button class="table-button delete-button" onclick="deleteExam(this)">✖</button>'; // Delete button
-        cell3.className = 'edit-cell'; // Corrected this line
-        cell4.className = 'delete-cell'; // Add this line
+        
+        // Insert the exam name into the selected slot and leave the other slot empty
+        if (slot === 'slot1') {
+            cell2.innerHTML = name;
+            cell3.innerHTML = '';
+        } else {
+            cell2.innerHTML = '';
+            cell3.innerHTML = name;
+        }
+        
+        cell4.innerHTML = '<button class="table-button edit-button" onclick="editExam(this)">✎</button>'; // Edit button
+        cell5.innerHTML = '<button class="table-button delete-button" onclick="deleteExam(this)">✖</button>'; // Delete button
+        // Reset the form fields
         document.getElementById('examDate').value = '';
         document.getElementById('examName').value = '';
+        document.getElementById('examSlot').value = 'slot1';
     } else {
         alert('Please fill both fields.');
     }
@@ -28,23 +40,27 @@ function addExam() {
 function editExam(button) {
     var row = button.parentNode.parentNode;
     var dateCell = row.cells[0];
-    var nameCell = row.cells[1];
-    var editButton = row.cells[2].firstChild;
+    var slot1Cell = row.cells[1];
+    var slot2Cell = row.cells[2];
+    var editButton = row.cells[3].firstChild;
 
     if (editButton.innerHTML === '✎') { // If the Edit button is clicked
         var existingDate = dateCell.innerHTML;
         var dateParts = existingDate.split("-");
         var formattedDate = dateParts[2] + "-" + dateParts[1] + "-" + dateParts[0]; // Reformatting date as YYYY-MM-DD
         dateCell.innerHTML = '<input type="date" value="' + formattedDate + '">';
-        nameCell.innerHTML = '<input type="text" value="' + nameCell.innerHTML + '">';
+        slot1Cell.innerHTML = '<input type="text" value="' + slot1Cell.innerHTML + '">';
+        slot2Cell.innerHTML = '<input type="text" value="' + slot2Cell.innerHTML + '">';
         editButton.innerHTML = '✔';
     } else { // If the Save button (✔) is clicked
         var newDate = dateCell.firstChild.value;
         var dateParts = newDate.split("-");
         newDate = dateParts[2] + "-" + dateParts[1] + "-" + dateParts[0]; // Reformatting date as DD-MM-YYYY
-        var newName = nameCell.firstChild.value;
+        var newSlot1Name = slot1Cell.firstChild.value;
+        var newSlot2Name = slot2Cell.firstChild.value;
         dateCell.innerHTML = newDate;
-        nameCell.innerHTML = newName;
+        slot1Cell.innerHTML = newSlot1Name;
+        slot2Cell.innerHTML = newSlot2Name;
         editButton.innerHTML = '✎';
     }
 }
@@ -56,17 +72,17 @@ function deleteExam(button) {
 
 function saveAsImage() {
     var table = document.getElementById('scheduleTable');
-    var editTh = table.querySelector('.edit-column'); // Select Edit header cell
-    var deleteTh = table.querySelector('.delete-column'); // Select Delete header cell
+    var editColumn = table.getElementsByClassName('edit-column')[0]; // Select Edit column
+    var deleteColumn = table.getElementsByClassName('delete-column')[0]; // Select Delete column
 
-    var editCells = table.querySelectorAll('.edit-cell');
-    var deleteCells = table.querySelectorAll('.delete-cell');
+    var editColumnIndex = editColumn.cellIndex;
+    var deleteColumnIndex = deleteColumn.cellIndex;
 
-    editTh.style.display = 'none'; // Hide Edit header cell
-    deleteTh.style.display = 'none'; // Hide Delete header cell
-
-    editCells.forEach(cell => cell.style.display = 'none'); // Hide the Edit cells
-    deleteCells.forEach(cell => cell.style.display = 'none'); // Hide the Delete cells
+    // Hide the Edit and Delete columns
+    Array.from(table.rows).forEach(row => {
+        row.cells[editColumnIndex].style.display = 'none';
+        row.cells[deleteColumnIndex].style.display = 'none';
+    });
 
     var node = document.getElementById('table-container');
     domtoimage.toJpeg(node, { quality: 1 })
@@ -76,19 +92,19 @@ function saveAsImage() {
             link.href = dataUrl;
             link.click();
 
-            editTh.style.display = ''; // Show Edit header cell again
-            deleteTh.style.display = ''; // Show Delete header cell again
-
-            editCells.forEach(cell => cell.style.display = ''); // Show the Edit cells again
-            deleteCells.forEach(cell => cell.style.display = ''); // Show the Delete cells again
+            // Show the Edit and Delete columns again
+            Array.from(table.rows).forEach(row => {
+                row.cells[editColumnIndex].style.display = '';
+                row.cells[deleteColumnIndex].style.display = '';
+            });
         })
         .catch(function (error) {
             console.error('An error occurred:', error);
 
-            editTh.style.display = ''; // Show Edit header cell again
-            deleteTh.style.display = ''; // Show Delete header cell again
-
-            editCells.forEach(cell => cell.style.display = ''); // Show the Edit cells again
-            deleteCells.forEach(cell => cell.style.display = ''); // Show the Delete cells again
+            // Show the Edit and Delete columns again in case of an error
+            Array.from(table.rows).forEach(row => {
+                row.cells[editColumnIndex].style.display = '';
+                row.cells[deleteColumnIndex].style.display = '';
+            });
         });
 }
